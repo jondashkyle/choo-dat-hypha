8619822d0432e86f4be3cc06f55b238f0015bda3
jondashkyle <contact@jon-kyle.com>
2018-01-02 14:23:57 -0800

    restructure

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ var path = require('path')
 
 module.exports = plugin
 
-function plugin (contentDir) {
+function plugin (contentDir, archive) {
+  archive = archive || createArchive()
   contentDir = contentDir || '/content'
+
   return async function store (state, emitter, app) {
     state.content = { }
-    state.loaded = false
+
+    state.hypha = {
+      error: '',
+      loaded: false,
+      isOwner: false,
+      p2p: false
+    }
 
     state.events.CONTENT_LOAD = 'content:load'
     emitter.on(state.events.CONTENT_LOAD, contentLoad)
@@ -17,20 +25,29 @@ function plugin (contentDir) {
     async function contentLoad (data) {
       try {
         state.content = await loadContent(contentDir)
-        state.p2p = true
+        state.hypha.loaded = true
       } catch (err) {
-        state.p2p = false
+        state.hypha.error = err.message
       }
-      state.loaded = true
+
       emitter.emit(state.events.RENDER)
     }
   }
-}
 
-async function loadContent (contentDir) {
-  var archive = new DatArchive(window.location.toString())
-  var options = { fs: archive, parent: contentDir }
-  var files = await archive.readdir(contentDir, { recursive: true })
-  var glob = files.map(function (file) { return path.join(contentDir, file) }) // funny hack
-  return hypha.readFiles(glob, contentDir, options)
+  function createArchive () {
+    try {
+      return new DatArchive(window.location.toString())
+      state.hypha.p2p = true
+    } catch (err) {
+      state.hypha.p2p = false
+      state.hypha.error = err.message
+    }
+  }
+
+  async function loadContent (contentDir) {
+    var options = { fs: archive, parent: contentDir }
+    var files = await archive.readdir(contentDir, { recursive: true })
+    var glob = files.map(function (file) { return path.join(contentDir, file) }) // funny hack
+    return hypha.readFiles(glob, contentDir, options)
+  }
 }
